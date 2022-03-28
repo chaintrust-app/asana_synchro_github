147e29ecd549e9cb80dfb0c5db565890976e80fb
root <root@DESKTOP-LJH8A3S.localdomain>
2022-03-28 14:26:50 +0200

    test 28

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/sh -l
 export ASANA_ACCESS_TOKEN=$ASANA_ACCESS_TOKEN
 echo 'rtejk'
-echo ${ secrets.ASANA_ACCESS_TOKEN }
-echo ${ steps.hello.outputs.time }
 echo $ASANA_ACCESS_TOKEN
 python asanaProject.py
