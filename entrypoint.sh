6ccac88b3b83c27bb0f84cfbbd72e0b4d1e6da7d
root <root@DESKTOP-LJH8A3S.localdomain>
2022-03-28 14:14:39 +0200

    test-26

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -l
 export ASANA_ACCESS_TOKEN=$ASANA_ACCESS_TOKEN
 echo 'rtejk'
-echo github.event.commits[0].message
+echo ${ secrets.ASANA_ACCESS_TOKEN }
+echo ${ steps.hello.outputs.time }
 echo $ASANA_ACCESS_TOKEN
 python asanaProject.py
