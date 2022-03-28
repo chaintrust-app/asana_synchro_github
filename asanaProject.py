4ccea10014999aa110e761f20d0f533de62462e5
root <root@DESKTOP-LJH8A3S.localdomain>
2022-03-28 17:41:28 +0200

    manque dernière étape et verif

--- asanaProject.py
+++ asanaProject.py
@@ -3,42 +3,62 @@ import os
 import re
 
 ################ info Github
-name_PR = os.environ['PR_NAME']
-new_state_of_task = 'In review'
+name_PR = '  OPS111 1- PR test de github' #os.environ['PR_NAME']
+new_state_of_task = 'In review'  ## a checker 
 
-#si plusieurs => faire bouger les 2
+################ info Asana => on récupère les différents projets au sein de chaintrust 
 
-################ info Asana
 client = asana.Client.access_token(os.environ['ASANA_ACCESS_TOKEN'])
-
-#l'espace chaintrust sur asana
 workspaces = client.workspaces.find_all()
-workspace_id = '1170802161750680'
+projects = client.projects.find_all({'workspace': list(workspaces)[0]["gid"]})
 
-#les différents projets au sein de chaintrust 
-projects = client.projects.find_all({'workspace': workspace_id})
+projects_list = ["Tech · Intra", "Tech · Extra", "Roadmap"] #verifier qu'on a accès 
 
-#Ici, je test sur tech-Intra
-tech_intra_id = 1201905678510288
-tasks = client.tasks.find_all({'project': tech_intra_id})
-sections = client.sections.find_by_project(tech_intra_id)
+
+def find_project_id_by_name(projects):
+    result_projects = []
+    for project_name in projects_list : 
+        if project_name == projects["name"]:
+            result_projects.append(projects['gid'])
+    return result_projects
+
+result_projects = list(filter(None, list(map(find_project_id_by_name, list(projects)))))
+print(result_projects)
+
+##si on a rien => ???
 
 ##################script 
 
-#On cherche la task associée sur asana 
+#valider le nom 
 
-###valider le nom 
-
+typology_name = ['ops', 'bus', 'cli']
 task_name = (''.join(re.search('(.+?)-', name_PR).group(1).split()))
 
-def find_task_id_by_name(tasks):
-    if task_name in tasks['name']:
-        return tasks['gid']
-    else : 
-        return ''
+for typo in typology_name:
+    if(typo in task_name.lower()):
+        print('coool')
     
-list_tasks_with_id = map(find_task_id_by_name, list(tasks))
-current_task = (''.join(list(list_tasks_with_id)))
+##sinon => ???
+
+
+#On cherche les tâches des différents projets
+
+def find_task_id_by_name(projects):
+    result_task = []
+    all_tasks = client.tasks.find_all({'project': projects})
+    print(projects)
+    for task in all_tasks: 
+        if task_name in task['name']:
+            result_task.append({task['gid']: projects})
+    print(result_task)
+    return result_task
+    
+result_task = filter(None, list(map(find_task_id_by_name, result_projects)))
+print(list(result_task)) #current task 
+
+#On cherche dans quel section sont les tâches et on les bouge 
+
+sections = client.sections.find_by_project(1201905678510288)
 
 #On cherche la colonne associé sur asana 
     
@@ -48,11 +68,12 @@ def find_state_id_by_name(sections):
     else : 
         return ''
 
-list_state_with_id = map(find_state_id_by_name, list(sections))
-new_state_current_task = (''.join(list(list_state_with_id)))
+result_state = map(find_state_id_by_name, list(sections))
+new_state_current_task = (''.join(list(result_state)))
 
 #on bouge la carte
-client.sections.add_task(new_state_current_task, {'task': current_task})
+
+#client.sections.add_task(new_state_current_task, {'task': current_task})
 
 
 #print(list(tasks))
