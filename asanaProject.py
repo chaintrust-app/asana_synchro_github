f650d9d87107a3838b5192713eaaeb7364581063
root <root@DESKTOP-LJH8A3S.localdomain>
2022-03-29 11:29:10 +0200

    OPS1111- V1 finish

--- asanaProject.py
+++ asanaProject.py
@@ -2,18 +2,15 @@ import asana
 import os 
 import re
 
-################ info Github
-name_PR = '  OPS111 1- PR test de github' #os.environ['PR_NAME']
-new_state_of_task = 'In review'  ## a checker 
-
-################ info Asana => on récupère les différents projets au sein de chaintrust 
+#init
 
 client = asana.Client.access_token(os.environ['ASANA_ACCESS_TOKEN'])
 workspaces = client.workspaces.find_all()
 projects = client.projects.find_all({'workspace': list(workspaces)[0]["gid"]})
 
-projects_list = ["Tech · Intra", "Tech · Extra", "Roadmap"] #verifier qu'on a accès 
+#fonctions
 
+#on récupère les différents projets au sein de chaintrust 
 
 def find_project_id_by_name(projects):
     result_projects = []
@@ -22,59 +19,58 @@ def find_project_id_by_name(projects):
             result_projects.append(projects['gid'])
     return result_projects
 
-result_projects = list(filter(None, list(map(find_project_id_by_name, list(projects)))))
-print(result_projects)
-
-##si on a rien => ???
-
-##################script 
-
-#valider le nom 
-
-typology_name = ['ops', 'bus', 'cli']
-task_name = (''.join(re.search('(.+?)-', name_PR).group(1).split()))
-
-for typo in typology_name:
-    if(typo in task_name.lower()):
-        print('coool')
-    
-##sinon => ???
-
-
 #On cherche les tâches des différents projets
 
 def find_task_id_by_name(projects):
     result_task = []
     all_tasks = client.tasks.find_all({'project': projects})
-    print(projects)
     for task in all_tasks: 
         if task_name in task['name']:
-            result_task.append({task['gid']: projects})
-    print(result_task)
+            result_task.append({'project': projects[0], 'task': task['gid']})
     return result_task
     
-result_task = filter(None, list(map(find_task_id_by_name, result_projects)))
-print(list(result_task)) #current task 
-
-#On cherche dans quel section sont les tâches et on les bouge 
-
-sections = client.sections.find_by_project(1201905678510288)
-
-#On cherche la colonne associé sur asana 
+#On cherche dans quelle section sont les tâches et on les bouge 
     
-def find_state_id_by_name(sections):
-    if new_state_of_task in sections['name']:
-        return sections['gid']
-    else : 
-        return ''
-
-result_state = map(find_state_id_by_name, list(sections))
-new_state_current_task = (''.join(list(result_state)))
-
-#on bouge la carte
-
-#client.sections.add_task(new_state_current_task, {'task': current_task})
+def find_state_id_by_name(tasks):
+    result_state = []
+    sections = client.sections.find_by_project(tasks['project'])
+    for section in sections:
+        if new_state_of_task in section['name']:
+            client.sections.add_task(section['gid'], {'task': tasks['task']})
+            result_state.append(task_name + ' move to ' + new_state_of_task)
+    return result_state
 
 
-#print(list(tasks))
-#print(list(sections))
+
+#Script 
+
+new_state_of_task = 'In review'  ## a checker 
+name_PR = '  OPS111 1- PR test de github' #os.environ['PR_NAME']
+task_name = (''.join(re.search('(.+?)-', name_PR).group(1).split()))
+
+#A modifier à la mano 
+typology_name = ['ops', 'bus', 'cli']
+projects_list = ["Tech · Intra", "Tech · Extra"] 
+
+
+def move_task():
+    for typo in typology_name:     #on check le nom de la task 
+        if(typo in task_name.lower()):
+            result_projects = list(filter(None, list(map(find_project_id_by_name, list(projects)))))
+            if result_projects == []: 
+                return "Vous n'avez accès à aucun projet sur asana"
+            else: 
+                result_task = list(filter(None, map(find_task_id_by_name, result_projects)))
+                if result_task == []: 
+                    return "Vous n'avez aucune tâche de ce nom dans vos projets asana"
+                else: 
+                    result = list(map(find_state_id_by_name, result_task[0]))
+                    if result == []:
+                        return "Aucune tâche n'a pu être bougée"
+                    else: 
+                        return result
+        else:
+            return 'le nom de la tâche n\'est pas valide'
+
+print(move_task())
+
